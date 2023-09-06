ddfc3b79a0d29a9c361433b9c2e431cb4eed330a
youssefahmed <moropass1212@gmail.com>
2023-09-06 17:53:30 +0300

    made few changes

--- js/main.js
+++ js/main.js
@@ -40,13 +40,6 @@
     });
 
 
-    // Facts counter
-    $('[data-toggle="counter-up"]').counterUp({
-        delay: 10,
-        time: 2000
-    });
-
-
     // Header carousel
     $(".header-carousel").owlCarousel({
         autoplay: true,
@@ -59,27 +52,7 @@
     });
 
 
-    // Testimonials carousel
-    $(".testimonial-carousel").owlCarousel({
-        autoplay: true,
-        smartSpeed: 1000,
-        center: true,
-        dots: false,
-        loop: true,
-        nav : true,
-        navText : [
-            '<i class="bi bi-arrow-left"></i>',
-            '<i class="bi bi-arrow-right"></i>'
-        ],
-        responsive: {
-            0:{
-                items:1
-            },
-            768:{
-                items:2
-            }
-        }
-    });
+   
 
 
     // Portfolio isotope and filter
